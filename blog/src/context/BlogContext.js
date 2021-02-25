f7fa8be879ad5c6ae55830e9694364e99d682a03
KYOUNGRAN <kyoungrankang@gmail.com>
2021-02-25 19:41:17 +0900

    ~139

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,24 +1,27 @@
-import React, { useState, createContext } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = createContext();
-
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
-
-  const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, 
+      { 
+        id: Math.floor(Math.random() * 99999), 
+        title: `Blog Post #${state.length + 1}` 
+      }
+    ];
+    default:
+      return state;
   }
-
-  // const blogPosts = [
-  //   { title: 'Blog Post #1' },
-  //   { title: 'Blog Post #2' }
-  // ];
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  )
 };
 
-export default BlogContext;
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
