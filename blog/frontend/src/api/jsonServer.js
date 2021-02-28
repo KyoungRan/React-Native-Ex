a60c319deca03234217c6a2c1a33dd1e9a66077f
KYOUNGRAN <kyoungrankang@gmail.com>
2021-02-28 13:04:53 +0900

    added Session 13: Data API Sync

---
+++ blog/frontend/src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://de8ca398e755.ngrok.io'
+});
--- blog/frontend/src/context/BlogContext.js
+++ blog/frontend/src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts': 
+      return action.payload;
     case 'add_blogpost':
       return [...state, 
       { 
@@ -25,21 +28,33 @@ const blogReducer = (state, action) => {
   }
 };
 
+const getBlogPosts = dispatch => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+    // response.data === [{}, {}, {}]
+    dispatch({ type: 'get_blogposts', payload: response.data });
+  }
+}
+
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost',  payload: { title, content }});
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content });
+    // dispatch({ type: 'add_blogpost',  payload: { title, content }});
     if(callback)  callback();
   }
 };
 
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: 'delete_blogpost', payload: id })  // id of post to delete
   };
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+
     dispatch({ 
       type: 'edit_blogpost', 
       payload: { id, title, content }
@@ -50,6 +65,6 @@ const editBlogPost = dispatch => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{id: 1, title: "A new pist", content:"what?"}]
+  { getBlogPosts, addBlogPost, deleteBlogPost, editBlogPost },
+  []
 );
