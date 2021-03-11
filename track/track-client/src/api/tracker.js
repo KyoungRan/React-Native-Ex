f7210e2c7f3af5201c8c195371d493ce2363617e
KYOUNGRAN <kyoungrankang@gmail.com>
2021-03-12 04:25:31 +0900

    241-246 Location

--- track/track-client/src/api/tracker.js
+++ track/track-client/src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://ef58ab2e38f2.ngrok.io'
+  baseURL: 'http://d0d9358155f9.ngrok.io'
 });
--- track/track-client/src/screens/TrackCreateScreen.js
+++ track/track-client/src/screens/TrackCreateScreen.js
@@ -1,40 +1,49 @@
-// import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import '../_mockLocation';
+import React, { useContext  } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
-import { SafeAreaView } from 'react-navigation';
-import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import { SafeAreaView, withNavigationFocus } from 'react-navigation';
+// import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
-  const [err, setErr] = useState(null);
+const TrackCreateScreen = ({ isFocused }) => {
+  const { addLocation } = useContext(LocationContext);
+  // same below: const [err] = useLocation((location) => addLocation(location));
+  const [err] = useLocation(isFocused, addLocation);
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      await watchPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        timeInterval: 1000,
-        distanceInterval: 10
-      }, (location) => {
-        console.log(location);
-      });
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-    } catch (e) {
-      setErr(e);
-    }
-  };
+  console.log(isFocused);
+  // const [err, setErr] = useState(null);
 
-  useEffect(() => {
-    startWatching();
-  }, []);
+  // const startWatching = async () => {
+  //   try {
+  //     const { granted } = await requestPermissionsAsync();
+  //     await watchPositionAsync({
+  //       accuracy: Accuracy.BestForNavigation,
+  //       timeInterval: 1000,
+  //       distanceInterval: 10
+  //     }, (location) => {
+  //       console.log(location);
+  //       addLocation(location);
+  //     });
+  //     if (!granted) {
+  //       throw new Error('Location permission not granted');
+  //     }
+  //   } catch (e) {
+  //     setErr(e);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   startWatching();
+  // }, []);
 
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>Create a Track</Text>
       <Map />
+      {/*<NavigationEvents onWillFocus={() => console.log('LEAVING')} />*/}
       {err ? <Text>Please enable location sercices</Text> : null}
     </SafeAreaView>
   )
@@ -45,4 +54,4 @@ const styles = StyleSheet.create({
 
 });
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
